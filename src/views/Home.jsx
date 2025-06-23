83d57398dbb866eb31f1da220cccb37f712e79ea
Your Name <you@example.com>
2025-06-23 03:09:00 -0500

    chaning cards role background color to white

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,20 +7,20 @@ function Home() {
   return (
     <div className="font-Rubik">
       <Header />
-      <section className="px-5 flex flex-col py-6 gap-4">
-        <h1 className="text-2xl">Selecciona tu rol</h1>
+      <section className="px-5 flex flex-col py-6 gap-4 text-carbon">
+        <h1 className="text-2xl text-white">Selecciona tu rol</h1>
 
         <Link
           to={"/client"}
-          className="border border-carbon/15 h-20 flex items-center gap-3 pr-2 "
+          className="h-22 flex items-center gap-3 pr-2 bg-white"
         >
-          <div className="p-3 bg-cosmic text-white aspect-square flex items-center justify-center h-20">
+          <div className="p-3 bg-cosmic text-white aspect-square flex items-center justify-center h-22">
             <span className="">
               <QrIcon sizes={40} />
             </span>
           </div>
           <div className="grow">
-            <h1 className="text-xl">Soy Cliente</h1>
+            <h1 className="text-xl font-medium">Soy Cliente</h1>
             <p>Genera tu QR gratis</p>
           </div>
           <span className="rounded-full bg-carbon/10 aspect-square p-3">
@@ -28,14 +28,14 @@ function Home() {
           </span>
         </Link>
 
-        <Link className="border border-carbon/15 h-20 flex items-center gap-3 pr-2">
-          <div className="p-3 bg-cosmic text-white aspect-square flex items-center justify-center h-20">
+        <Link className="h-22 flex items-center gap-3 pr-2 bg-white">
+          <div className="p-3 bg-cosmic text-white aspect-square flex items-center justify-center h-22">
             <span className="">
               <QrIcon sizes={40} />
             </span>
           </div>
           <div className="grow">
-            <h1 className="text-xl">Soy Dueño</h1>
+            <h1 className="text-xl font-medium">Soy Dueño</h1>
             <p>Registrate en segundos</p>
           </div>
           <span className="rounded-full bg-carbon/10 aspect-square p-3">
